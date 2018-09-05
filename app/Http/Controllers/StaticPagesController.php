684eaaee1fe573f9322f6fb13f20a402aab4afbd
vagrant <vagrant@homestead>
2018-09-05 15:49:33 +0000

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //
+    /*
+    public function home()
+    {
+        return '主页';
+    }
+
+    public function help()
+    {
+        return '帮助页';
+    }
+
+    public function about()
+    {
+        return '关于页';
+    }
+    */
+    public function home()
+    {
+        return view('static_pages/home');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
+}
